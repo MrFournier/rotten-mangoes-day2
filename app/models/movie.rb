d1cd53aea7c080cfa0663b3b7cee2a90c595504c
MrFournier <troy_fournier@live.com>
2016-07-22 01:08:36 +0000

    Refactored the logic for searching moving it from movie controller to the movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -25,6 +25,25 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_past
 
+  # Logic to search through movies in the db
+  scope :search_title, -> (title) { where('title LIKE ?', "%#{title}%") if title.present? }
+  scope :search_director, -> (director) { where('director LIKE ?', "%#{director}%") if director.present? }
+  scope :search_runtime, -> (runtime_in_minutes) { 
+
+    if runtime_in_minutes
+      
+      case runtime_in_minutes
+
+      when '1'
+        where('runtime_in_minutes < ?', 90)
+      when '2'
+        where(runtime_in_minutes: 90..120)
+      when '3'
+        where('runtime_in_minutes > ?', 120)
+      end
+    end
+  }
+
   def review_average
     reviews.sum(:rating_out_of_ten) / reviews.size unless reviews.empty?
   end
